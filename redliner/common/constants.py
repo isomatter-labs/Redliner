d1e26f8a7c8446bd9a3b15d2e7d02c36daa629db
Caleb Jett <calebthejett@gmail.com>
2025-08-06 22:17:58 -0500

    Fix repo references

--- redliner/common/constants.py
+++ redliner/common/constants.py
@@ -6,4 +6,4 @@ PREVIEW_DPI = 10
 PREVIEW_SIZE = qtc.QSize(128, 128)
 GRAYSCALE_WEIGHTS = np.array([0.2989, 0.5870, 0.1140])
 VERSION_PATTERN = re.compile(r'\[(\d+\.\d+\.\d+)\]')
-REMOTE_CHANGELOG = r"https://raw.githubusercontent.com/isomatter-labs/redliner/refs/heads/main/CHANGELOG.md"
+REMOTE_CHANGELOG = r"https://raw.githubusercontent.com/CJett/Redliner/refs/heads/main/CHANGELOG.md"
